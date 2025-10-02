412a8200ea82e693dc1c9c1409e77022228c6b86
chattrintun <chattrin.tun@student.mahidol.edu>
2025-10-02 04:33:04 +0000

    done

--- main.cpp
+++ main.cpp
@@ -5,34 +5,49 @@
 using namespace std;
 
 int main(int argc,char *argv[]){
-  int *pa, *pb, i, temp;
-int a[SIZE] ={1,2,3,4,5,6,7,8,9,10}; 
-pa = &a[0]; //pa =a;
+  int *pa, *pb, i, temp, *p;
+//int a[SIZE] ={1,2,3,4,5,6,7,8,9,10}; remove
+int n;
+cout<<"How many numbers?";
+cin>>n;
+pa= new int[n];
+for(i=0; i<n; i++){
+  cin>>*pa;
+  pa++;
+}
+pa-=n;
+
+
+// print veryhing
+
+
 cout<<"Original: ";
-for (i=0; i<SIZE-1; i++ ,pa++){
-   cout<<setw(3)<<*pa ;//<<endl;
-  //cout<<pa<<endl;
+for(i=0; i<n-1; i++){
+  cout<<setw(3)<<*pa;
+  pa++;
 }
-cout<<setw(3)<<*pa<<endl;
-
-pa = &a[0];  
-pb = &a[SIZE-1];
-for (i=0; i<SIZE/2; i++)
+cout<<setw(3)<<*pa;
+// print reverse
+cout<< endl <<"Reverse:  ";
+pb=pa;
+pa-=(n-1);
+for (i=0; i<n/2; i++)
 {
-   temp = *pa;  
-   *pa = *pb;  
-   *pb = temp;
-   pa++;  pb--;
+  temp = *pa;
+  *pa=*pb;
+  *pb = temp;
+  pa++; pb--;
 }
-pa=&a[0];
-pb-=SIZE/2;
-cout<<"Reversed: ";
-for (i=0; i<SIZE-1; i++ ,pa++){
-   cout<<setw(3)<<*pa ;//<<endl;
-  //cout<<pa<<endl;
+pa -=n/2;
+for(i=0; i<n-1; i++){
+  cout<<setw(3)<<*pa;
+  pa++;
 }
-cout<<setw(3)<<*pa<<endl;
+cout<<setw(3)<<*pa;
 
+cout<< endl;
+pa -=(n-1);
+delete []pa;
 
 return 0;
 }
